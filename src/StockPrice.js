b84dbd3058159fa7a9378f3cef3a7153ef4c684f
Hunter M <huntermason15@gmail.com>
2023-08-19 18:40:48 -0400

    Adding user input

--- src/StockPrice.js
+++ src/StockPrice.js
@@ -9,54 +9,58 @@ const StockPrice = () => {
     setSymbols(event.target.value);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (symbols) {
-        const options = {
-          method: 'GET',
-          url: 'https://twelve-data1.p.rapidapi.com/price',
-          params: {
-            symbol: symbols,
-            format: 'json',
-            outputsize: '30'
-          },
-          headers: {
-            'X-RapidAPI-Key': '577a69f858msh40fe029fdfb0c7bp1d982cjsna5b05d487d92',
-            'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
-          }
-        };
-
-        try {
-          const response = await axios.request(options);
-          setPrices(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
     fetchData();
-  }, [symbols]);
+  };
+
+  const fetchData = async () => {
+    if (symbols) {
+      const options = {
+        method: 'GET',
+        url: 'https://twelve-data1.p.rapidapi.com/price',
+        params: {
+          symbol: symbols,
+          format: 'json',
+          outputsize: '30'
+        },
+        headers: {
+          'X-RapidAPI-Key': '577a69f858msh40fe029fdfb0c7bp1d982cjsna5b05d487d92',
+          'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        setPrices(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <div>
-      <h1 className=' drop-shadow-md' >Stock Prices</h1>
-      <div className='flex flex-row justify-center container-fluid'>
-        <div className='flex-col stock-prices'>         
+      <h1 className='drop-shadow-md'>Stock Prices</h1>
+      <div className='flex flex-row justify-center'>
+        <form onSubmit={handleSubmit} className='flex-col stock-prices'>
           <input
             type="text"
             id='stockSymbols'
             value={symbols}
             className='text-center text-purple-50 bg-purple-600 hover:bg-purple-500 rounded shadow-sm shadow-black'
             onChange={handleInputChange}
-            placeholder="Enter symbols here"
+            placeholder="Enter Symbols"
           />
-        </div>
+          <button type="submit" className='rounded shadow-md p-2 mt-2'>
+            Fetch Prices
+          </button>
+        </form>
       </div>
-      <ul>
+      <ul className='text-center'>
         {Object.keys(prices).map(symbol => (
-          <li key={symbol}>
-            {symbol}: {prices[symbol]}
+          <li key={symbols}>
+            {symbols}: {prices[symbol]}
           </li>
         ))}
       </ul>
