aea2adb4245159d76eef35786041edacb1cdc464
Hunter M <huntermason15@gmail.com>
2023-09-27 18:47:38 -0400

    Screen size Tailwind Config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'min': '767px'},
+      // => @media (min-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
   },
   plugins: [],
 }
