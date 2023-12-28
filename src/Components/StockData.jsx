5ce974c975b944f96a25789d368ffa18c5f8d9c8
Hunter M <huntermason15@gmail.com>
2023-12-28 16:08:09 -0500

    More configurations to breeze, laravel, and tailwind.

--- src/Components/StockData.jsx
+++ src/Components/StockData.jsx
@@ -4,10 +4,10 @@ import DetailedStockData from "./DetailedStockData";
 const StockData = ({ symbol, prices }) => {
     const [selectedSymbol, setSelectedSymbol] = useState({});
 
-    //On Click function for the button to fire detailedStockData 
+    //On Click function for the button to fire detailedStockData
     const handleButtonClick = (symbol) => {
         setSelectedSymbol((prevSelectedSymbols) => {
-            //Messy way to handle the state of which symbol was clicked. Checks to see if the symbol exists in prevSelectedSymbols, adds it to the array if it doesn't. 
+            //Messy way to handle the state of which symbol was clicked. Checks to see if the symbol exists in prevSelectedSymbols, adds it to the array if it doesn't.
             // Should probably re-work this in the future.
             return { ...prevSelectedSymbols, [symbol]: symbol };
         });
@@ -25,9 +25,9 @@ const StockData = ({ symbol, prices }) => {
 
     return (
         <>
-        {/* 
+        {/*
             Parent Div on StockPrice exists already and class id is 'stockPrices, reason for using an empty JSX parent element'.
-      
+
             'CARD' FOR DISPLAYING ALL OF THE STOCK INFORMATION
         */}
         <h1 className="mt-6 text-center font-bold text-2xl">
@@ -37,19 +37,19 @@ const StockData = ({ symbol, prices }) => {
         <div className="lg:flex lg:flex-col lg:justify-center">
             {rows.map((row, rowIndex) => (
             <div key={`row-${rowIndex}`} className="grid grid-cols-1 gap-8 mt-8 sm:grid-cols-3">
-                {row.map((symbols) => (             
+                {row.map((symbols) => (
                 <div
                     key={symbols}
                     id={symbols === "price" ? symbol : symbols}
                     className={`${
                         rowIndex % 2 === 0 ? "bg-indigo-200" : "bg-indigo-300"
-                        } p-4 shadow space-y-2 duration-200 transition-all rounded-sm hover:shadow-lg`}       
-                >   
+                        } p-4 shadow space-y-2 duration-200 transition-all rounded-sm hover:shadow-lg`}
+                >
                     {/* Symbol Column */}
                     <div className="flex justify-center" id="symbol">
                         <div className="uppercase font-medium">
                             {symbols === "price" ? symbol : symbols}
-                            {/* 
+                            {/*
                                 If price doesn't exist, then there was only one symbol input by the user, changes the way data must be referenced.
                                 Checking length of symbols here doesn't help since the data structure is different when there is one vs multiple symbols submitted to the API.
 
@@ -67,7 +67,7 @@ const StockData = ({ symbol, prices }) => {
                     </div>
                     {/* Below is very messy and should be reworked. I have nested ternaries to set classes, could potentialy clean this up by using clsx */}
                     <div className="flex flex-col justify-center ">
-                        <button className={`${isButtonHidden(symbols === "price" ? symbol : symbols) ? "hidden" : ""} transition-all duration-150 ease-in-out`} 
+                        <button className={`${isButtonHidden(symbols === "price" ? symbol : symbols) ? "hidden" : ""} transition-all duration-150 ease-in-out`}
                                 onClick={() => handleButtonClick(symbols === "price" ? symbol : symbols)}
                         >
                             More..
